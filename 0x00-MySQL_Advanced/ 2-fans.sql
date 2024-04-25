aea2c3282a2d20bdeb42e87821c1a9bfe16d1747
G-omar-H <omarghazi12@gmail.com>
2024-04-25 15:53:57 +0100

    2. Best band ever!

--- 0x00-MySQL_Advanced/ 2-fans.sql
+++ 0x00-MySQL_Advanced/ 2-fans.sql
@@ -1,7 +1,7 @@
 -- 2-fans.sql
 -- SQL script that ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
-SELECT  origin, count(*) 
+SELECT  origin, SUM(fans)
 AS nb_fans FROM metal_bands 
 GROUP BY origin 
 ORDER BY nb_fans DESC;
