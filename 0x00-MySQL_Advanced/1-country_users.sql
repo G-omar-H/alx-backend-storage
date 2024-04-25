b7170df855c066b2e531eea62d1da7d82b63e7bd
G-omar-H <omarghazi12@gmail.com>
2024-04-25 15:00:51 +0100

    1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,15 @@
+--  1-country_users.sql
+-- QL script that creates a table users following these requirements:
+--
+--    With these attributes:
+--        id, integer, never null, auto increment and primary key
+--        email, string (255 characters), never null and unique
+--        name, string (255 characters)
+--   If the table already exists, the  script should not fail
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email CHAR(255) NOT NULL UNIQUE,
+    name CHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
