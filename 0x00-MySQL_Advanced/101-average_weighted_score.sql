cd44f307accf8ec72ebeb971a2984d8881d82d3a
G-omar-H <omarghazi12@gmail.com>
2024-04-27 11:37:47 +0100

    13. Average weighted score for all!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,18 +1,41 @@
--- 101-average_weighted_score.sql
-
--- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes
--- and store the average weighted score for all students.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER |
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-   UPDATE users
-   JOIN (
-    SELECT user_id, SUM(corrections.score * projects.weight) / SUM(projects.weight) AS weighted_average
-    FROM corrections 
-    JOIN projects ON projects.id = corrections.project_id
-    GROUP BY user_id
-   ) AS weighted_average ON users.id = weighted_average.user_id
-   SET users.average_score = weighted_average.weighted_average;
+    DECLARE user_id INT;
+    DECLARE average_weighted_score FLOAT;
+
+    DECLARE done INT DEFAULT 0;
+    -- Cursor to iterate over each user
+    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    -- Variable to control loop termination
+
+    -- Open cursor
+    OPEN user_cursor;
+
+    -- Loop through each user
+    user_loop: LOOP
+        -- Fetch user ID
+        FETCH user_cursor INTO user_id;
+        IF done = 1 THEN
+            LEAVE user_loop;
+        END IF;
+
+        -- Calculate average weighted score for the user
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight) INTO average_weighted_score
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+        -- Update users table with the calculated average weighted score
+        UPDATE users SET average_score = average_weighted_score WHERE id = user_id;
+    END LOOP;
+
+    -- Close cursor
+    CLOSE user_cursor;
 END |
+
 DELIMITER ;
