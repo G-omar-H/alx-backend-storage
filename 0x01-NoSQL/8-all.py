d96c29a1aa189851419a00fc4f415be33f35a0eb
G-omar-H <omarghazi12@gmail.com>
2024-04-30 13:24:34 +0100

    8. List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,9 +5,10 @@ Python function that lists all documents in a collection
 
 import pymongo
 
+
 def list_all(mongo_collection):
     """
-    
+
 
     Args:
         mongo_collection (_type_): _description_
