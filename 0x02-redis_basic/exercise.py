84604b2498da21a695160b3db7ee070e22714959
G-omar-H <omarghazi12@gmail.com>
2024-05-01 19:47:17 +0100

    2. Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,34 @@ Main file
 """
 import redis
 import uuid
-from typing import Union, Optional
+from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    decorator to count number of times a Cash class method is called
+
+    Args:
+        method (Callable): _description_
+
+    Returns:
+        Callable: _description_
+    """
+
+    @wraps(method)
+    def count(self, *args, **kwargs):
+        """
+        decorater wrapp function to increment te count
+
+        Returns:
+            int _: count
+        """
+        key = method.__qualname__
+        self._redis.incr(key, 1)
+        return method(self, *args, **kwargs)
+
+    return count
 
 
 class Cache:
@@ -19,6 +46,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis using a random key
@@ -32,8 +60,8 @@ class Cache:
         return key
 
     def get(
-           self, key: str, fn: Optional[callable] = None
-           ) -> Union[int, str, bytes, float]:
+        self, key: str, fn: Optional[callable] = None
+    ) -> Union[int, str, bytes, float]:
         """
         get method that take a key string argument and an
         optional Callable argument named fn.
